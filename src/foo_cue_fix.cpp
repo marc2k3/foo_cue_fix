d583a3a4adf27e79495f55dc509d1a021c2df149
marc2k3 <marc2003@live.co.uk>
2022-09-12 14:54:46 +0100

    Use VALIDATE_COMPONENT_FILENAME instead of component_installation_validator

--- src/foo_cue_fix.cpp
+++ src/foo_cue_fix.cpp
@@ -17,17 +17,8 @@ namespace
 		"Copyright (C) 2022 marc2003\n\n"
 		"Build: " __TIME__ ", " __DATE__
 	);
-	
-	class InstallationValidator : public component_installation_validator
-	{
-	public:
-		bool is_installed_correctly() override
-		{
-			return test_my_name("foo_cue_fix.dll") && core_version_info_v2::get()->test_version(2, 0, 0, 0);
-		}
-	};
 
-	FB2K_SERVICE_FACTORY(InstallationValidator);
+	VALIDATE_COMPONENT_FILENAME("foo_cue_fix.dll");
 
 	class CueFix : public threaded_process_callback
 	{
